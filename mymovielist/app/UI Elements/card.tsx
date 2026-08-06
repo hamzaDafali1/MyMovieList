a0852066b9a1ea755fcaca5024334d9f3b7afd5a
Hamza Dafali <68519037+hamzaDafali1@users.noreply.github.com>
2026-08-06 14:46:53 +0000

    working on card

---
+++ mymovielist/app/UI Elements/card.tsx
@@ -0,0 +1,26 @@
+
+
+export default function Card(){
+    return (
+        <div className="flex flex-col items-center gap-6 rounded-2xl border-solid border-2 border-white-200 w-fit rounded-none text-md">
+        <div className="flex flex-col">
+                <img className="w-fit
+" src="https://media.themoviedb.org/t/p/w220_and_h330_face/iPOn6DinuVyLY17YM9mKuPofV08.jpg" />
+            <div className="flex flex-col  py-9 px-3">
+                <h1 className="text-xl">Title</h1>
+                <div className="flex gap-2 font-medium text-gray-600 dark:text-gray-400 gap-y-8">
+                    <span className="text-purple-300">2026 . </span>
+                    <span className="text-purple-300">4.8/5 . </span>
+                    <span className="text-purple-300">Comedy</span>
+                </div>
+                <p className="text-wrap w-xs text-md">A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.
+</p>
+                <div className="flex gap-5 font-medium">
+                <button className="border-1 border-white-200 bg-white text-black p-2">Watched</button>
+                <button className="border-1 border-white-200 bg-white text-black p-2">Watch later</button>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
