3165f18c9ae657353019b991db6bb84a1caff02f
Hamza Dafali <68519037+hamzaDafali1@users.noreply.github.com>
2026-08-15 15:24:10 +0000

    working on extra info

--- mymovielist/app/movie/[id]/page.tsx
+++ mymovielist/app/movie/[id]/page.tsx
@@ -1,5 +1,6 @@
 import CastSlideShow from "@/app/ui/movie/castslideshow";
 import InfoCard from "@/app/ui/movie/infoCard";
+import ExtraInfo from "@/app/ui/movie/extraInfo";
 
 export default async function Page(props: { params: Promise<{ id: number }> }) {
   const params = await props.params;
@@ -15,54 +16,8 @@ export default async function Page(props: { params: Promise<{ id: number }> }) {
           <h5 className="text-3xl flex flex-left w-fit">Cast</h5>
           <CastSlideShow id={id} />
         </div>
-
-
-
-
-
-
         <div className="flex flex-col w-1/2 pl-10">
-          <div>
-            <h1 className="text-4xl text-white font-bold">
-              qsdsq
-              <span className="text-gray-200 font-normal">qsdqsd</span>
-            </h1>
-          </div>
-
-          <div>
-            <span className="text-lg text-white font-bold bg-violet-500 inline-block rounded-lg p-1 m-1 mt-3">
-              qsdq
-            </span>
-            <span className="font-semibold">sqddfs</span>
-          </div>
-          <div className="flex flew-row gap-2">
-            <a
-              href="#"
-              className="w-10 text-lg text-white font-bold bg-black inline-block rounded-full p-2 m-1 mt-3"
-            ></a>
-            <a
-              href="#"
-              className="w-10 text-lg text-white font-bold bg-black inline-block rounded-full p-2 m-1 mt-3"
-            ></a>
-
-            <a
-              href="#"
-              className="w-10 text-lg text-white font-bold bg-black inline-block rounded-full p-2 m-1 mt-3"
-            ></a>
-          </div>
-          <div>
-            <span className="text-lg text-white italic text-gray-200 inline-block rounded-lg p-1 m-1 ">
-              qsddsff
-            </span>
-          </div>
-          <div>
-            <span className="text-lg text-white text-gray-200 inline-block rounded-lg p-1 m-1 mt-3">
-              Overview
-            </span>
-            <span className="text-md text-white text-gray-200 inline-block rounded-lg p-1 m-1 mt-3 text-wrap">
-              qsdsf
-            </span>
-          </div>
+          <ExtraInfo id={5} />
         </div>
       </div>
     </div>
--- mymovielist/app/ui/movie/extraInfo.tsx
+++ mymovielist/app/ui/movie/extraInfo.tsx
@@ -2,8 +2,48 @@ export default async function ExtraInfo({id} : {id : number}){
 
 
     return (
+      <div className="h-full">
         <div>
-            
+          <h1 className="text-4xl text-white font-bold">
+            qsdsq
+            <span className="text-gray-200 font-normal">qsdqsd</span>
+          </h1>
         </div>
-    )
+
+        <div>
+          <span className="text-lg text-white font-bold bg-violet-500 inline-block rounded-lg p-1 m-1 mt-3">
+            qsdq
+          </span>
+          <span className="font-semibold">sqddfs</span>
+        </div>
+        <div className="flex flew-row gap-2">
+          <a
+            href="#"
+            className="w-10 text-lg text-white font-bold bg-black inline-block rounded-full p-2 m-1 mt-3"
+          ></a>
+          <a
+            href="#"
+            className="w-10 text-lg text-white font-bold bg-black inline-block rounded-full p-2 m-1 mt-3"
+          ></a>
+
+          <a
+            href="#"
+            className="w-10 text-lg text-white font-bold bg-black inline-block rounded-full p-2 m-1 mt-3"
+          ></a>
+        </div>
+        <div>
+          <span className="text-lg text-white italic text-gray-200 inline-block rounded-lg p-1 m-1 ">
+            qsddsff
+          </span>
+        </div>
+        <div>
+          <span className="text-lg text-white text-gray-200 inline-block rounded-lg p-1 m-1 mt-3">
+            Overview
+          </span>
+          <span className="text-md text-white text-gray-200 inline-block rounded-lg p-1 m-1 mt-3 text-wrap">
+            qsdsf
+          </span>
+        </div>
+      </div>
+    );
 }
